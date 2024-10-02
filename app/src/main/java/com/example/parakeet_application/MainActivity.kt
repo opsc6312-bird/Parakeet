43460485ad74df57651d4dc55cabdca819603d0f
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-10-02 18:51:23 +0200

    "NavigationBar: The navigationBar is complete"

--- app/src/main/java/com/example/parakeet_application/MainActivity.kt
+++ app/src/main/java/com/example/parakeet_application/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.example.parakeet_application.databinding.ActivityMainBinding
@@ -12,6 +13,10 @@ import com.example.parakeet_application.databinding.ToolbarLayoutBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -45,7 +50,40 @@ class MainActivity : AppCompatActivity() {
         toggle.syncState()
         val navController = Navigation.findNavController(this, R.id.fragmentContainer)
         NavigationUI.setupWithNavController(
-
+            navigationDrawerLayoutBinding.navView,
+            navController
         )
+        val headerLayout = navigationDrawerLayoutBinding.navView.getHeaderView(0)
+        imgHeader = headerLayout.findViewById(R.id.imgHeader)
+        txtName = headerLayout.findViewById(R.id.txtHeaderName)
+        txtEmail = headerLayout.findViewById(R.id.txtHeaderEmail)
+    }
+
+    @Deprecated("Deprecated in Java",
+        ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
+    )
+    override fun onBackPressed() {
+        if (navigationDrawerLayoutBinding.navDrawer.isDrawerOpen(GravityCompat.START))
+            navigationDrawerLayoutBinding.navDrawer.closeDrawer(GravityCompat.START)
+        else
+            super.onBackPressed()
+    }
+    private fun gerUserData(){
+        val database = Firebase.database.getReference("Users").child(firebaseAuth.uid!!)
+        database.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    val userModel = snapshot.getValue(UserModel::class.java)
+                    Glide.with(this@MainActivity).load(userModel?.image).into(imgHeader)
+                    txtName.text = userModel?.username
+                    txtEmail.text = userModel?.email
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        });
     }
 }
