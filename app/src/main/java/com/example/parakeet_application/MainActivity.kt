6cee9f2542048c524eb0ce9e295d986be92d87b0
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-10-02 17:39:55 +0200

    Almost there

--- app/src/main/java/com/example/parakeet_application/MainActivity.kt
+++ app/src/main/java/com/example/parakeet_application/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.parakeet_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.parakeet_application.databinding.ActivityMainBinding
+import com.example.parakeet_application.databinding.NavigationDrawerLayoutBinding
+import com.example.parakeet_application.databinding.ToolbarLayoutBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navigationDrawerLayoutBinding: NavigationDrawerLayoutBinding
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var toolbarLayoutBinding: ToolbarLayoutBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var imgHeader: CircleImageView
+    private lateinit var txtName: TextView
+    private lateinit var txtEmail: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        navigationDrawerLayoutBinding = NavigationDrawerLayoutBinding.inflate(layoutInflater)
+        setContentView(navigationDrawerLayoutBinding.root)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        toolbarLayoutBinding = mainBinding.toolbar
+
+        setSupportActionBar(toolbarLayoutBinding.toolbar)
+
+        firebaseAuth = Firebase.auth
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            navigationDrawerLayoutBinding.navDrawer,
+            toolbarLayoutBinding.toolbar,
+            R.string.open_navigation_drawer,
+            R.string.close_navigation_drawer
+        )
+        navigationDrawerLayoutBinding.navDrawer.addDrawerListener(toggle)
+        toggle.syncState()
+        val navController = Navigation.findNavController(this, R.id.fragmentContainer)
+        NavigationUI.setupWithNavController(
+
+        )
     }
 }
