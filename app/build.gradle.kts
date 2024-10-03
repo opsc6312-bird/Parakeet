58addaf4f780c12db88424b864cacac7d2ca2bee
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-10-03 19:45:38 +0200

    Init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.2")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -93,4 +93,14 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.8.4")
     //Circular Indicator library
     //implementation("com.victor:lib:1.0.4")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0");
+    implementation("com.squareup.retrofit2:converter-gson:2.10.0");
+    // define a BOM and its version
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+
+    // define any required OkHttp artifacts without version
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
